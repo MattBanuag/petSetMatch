cc2a2133f0bad7c042e8d35f413a3bb4e8da8e3b
MattBanuag <matthewbanuag.dev@gmail.com>
2023-06-02 15:12:58 -0500

    Update: Banner component logic by passing in props

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,12 @@
 import Button from './Button';
 
-function Banner() {
+function Banner(props) {
   return (
     <section id="banner">
         <article>
-            <h2>Welcome to PetSetMatch!</h2>
+            <h2>{props.title}</h2>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-                officia deserunt mollit anim id est laborum.
+                {props.text}
             </p>
         </article>
 
