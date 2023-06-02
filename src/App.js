d116b1e84c5f3221325f4d310228ea53aaae334b
MattBanuag <matthewbanuag.dev@gmail.com>
2023-06-02 12:54:57 -0500

    Add: Banner component, design and layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './style/index.css';
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <main className="container">
       <Header/>
+      <Banner/>
     </main>
   );
 }
