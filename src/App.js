fd23d1920137f95767129871172a39ded388075c
MattBanuag <matthewbanuag.dev@gmail.com>
2023-06-02 14:59:59 -0500

    Add: Footer component, design and layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './style/index.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Gallery from './components/Gallery';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Banner/>
       <Gallery/>
+      <Footer/>
     </main>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+        <article>
+            <p>&copy; 2023 PetSetMatch.</p>
+            <p>All rights reserved.</p>
+        </article>
+    </footer>
+  )
+}
+
+export default Footer;
